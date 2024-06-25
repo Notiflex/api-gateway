bf179a393c2cd39931b55fe80b2b0bdb490a6bfb
gospel306 <gospel306@gmail.com>
2024-06-25 20:02:16 +0900

    api-gateway init

---
+++ src/test/java/com/notiflex/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notiflex.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
